8705ebe26dff760fd17dcc22bed8285c07fb0d49
javi112214 <rst726gmnx@privaterelay.appleid.com>
2026-03-31 10:57:38 -0300

    Create js/guides-data.js with example guide data

---
+++ js/guides-data.js
@@ -0,0 +1,19 @@
+// Example guide data for RaiderBase
+module.exports = {
+    guides: [
+        {
+            id: 1,
+            title: 'Getting Started with RaiderBase',
+            content: 'This guide provides an introduction to using RaiderBase effectively.',
+            created_at: '2026-03-31 13:52:58',
+            author: 'javi112214'
+        },
+        {
+            id: 2,
+            title: 'Advanced Tips for RaiderBase',
+            content: 'This guide offers advanced tips for power users of RaiderBase.',
+            created_at: '2026-03-31 13:52:58',
+            author: 'javi112214'
+        }
+    ]
+};
