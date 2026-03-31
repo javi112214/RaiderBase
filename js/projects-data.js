57594be71ec8cc85f753f61690a35133e05015b6
javi112214 <rst726gmnx@privaterelay.appleid.com>
2026-03-31 10:53:28 -0300

    Add example projects data to projects-data.js

---
+++ js/projects-data.js
@@ -0,0 +1,28 @@
+const projectsData = [
+    {
+        id: 1,
+        name: 'Project Alpha',
+        description: 'This is a description for Project Alpha.',
+        url: 'https://example.com/project-alpha',
+        dateCreated: '2026-03-31',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Project Beta',
+        description: 'This is a description for Project Beta.',
+        url: 'https://example.com/project-beta',
+        dateCreated: '2026-03-31',
+        status: 'completed'
+    },
+    {
+        id: 3,
+        name: 'Project Gamma',
+        description: 'This is a description for Project Gamma.',
+        url: 'https://example.com/project-gamma',
+        dateCreated: '2026-03-31',
+        status: 'inactive'
+    }
+];
+
+export default projectsData;
