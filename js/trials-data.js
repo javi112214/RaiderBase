4255e7f9767bfb5d5a26905c2a46efbfa7722921
javi112214 <rst726gmnx@privaterelay.appleid.com>
2026-03-31 10:56:11 -0300

    Create js/trials-data.js with example weekly trials data

---
+++ js/trials-data.js
@@ -0,0 +1,32 @@
+const weeklyTrialsData = [
+    {
+        week: 1,
+        trials: [
+            { trialID: 1, date: '2026-03-01', participants: 50, duration: 120 },
+            { trialID: 2, date: '2026-03-02', participants: 45, duration: 150 }
+        ]
+    },
+    {
+        week: 2,
+        trials: [
+            { trialID: 3, date: '2026-03-08', participants: 30, duration: 90 },
+            { trialID: 4, date: '2026-03-09', participants: 60, duration: 180 }
+        ]
+    },
+    {
+        week: 3,
+        trials: [
+            { trialID: 5, date: '2026-03-15', participants: 55, duration: 200 },
+            { trialID: 6, date: '2026-03-16', participants: 40, duration: 150 }
+        ]
+    },
+    {
+        week: 4,
+        trials: [
+            { trialID: 7, date: '2026-03-22', participants: 70, duration: 240 },
+            { trialID: 8, date: '2026-03-23', participants: 65, duration: 180 }
+        ]
+    }
+];
+
+module.exports = weeklyTrialsData;
